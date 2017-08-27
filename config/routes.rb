62adbeb41f5ef3e78e5a105f6a7ffa5592151568
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-08-27 18:04:17 +0530

    Setup users controller and list users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   resources :products
   devise_for :users
   root to: 'page#home'
   get '/about',to: 'page#about'
-
 resources :categories, except: [:delete, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
