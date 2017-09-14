8dc731d23d689ea89d35ea53d64e62aa4160793f
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-09-14 19:02:30 +0530

    Minor Changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_params, if: :devise_controller?
 
   def configure_permitted_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :link])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :link])
+
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-has_many :products, dependent: :destroy
+  has_many :products, dependent: :destroy
+  validates_presence_of :link
 end
